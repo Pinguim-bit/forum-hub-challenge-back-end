65754881c65ab44875ce2e00d38e8495e2f2b063
Ryan Marcello <ryan.rmasa@gmail.com>
2026-03-01 12:12:18 -0300

    first commit

---
+++ src/test/java/com/pinguimbit/forumHubChallengeBackEnd/ForumHubChallengeBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinguimbit.forumHubChallengeBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumHubChallengeBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
